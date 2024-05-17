605628ca3d6b2ae5199f6f4940c92c140911488c
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-05-17 12:14:56 +0600

    yt

--- src/components/Home/BannerCategory.jsx
+++ src/components/Home/BannerCategory.jsx
@@ -2,7 +2,7 @@ import { category } from "../../utils/constants";
 const BannerCategory = () => {
   console.log(category);
   return (
-    <div className=" bg-white rounded-2xl px-6 text-md pt-4">
+    <div className=" bg-white rounded-2xl px-6 text-md pt-4 border-[1px] border-gray-400">
       {category.map((cat) => (
         <>
           <div
