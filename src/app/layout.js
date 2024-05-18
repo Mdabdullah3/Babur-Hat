53ff58bb1a7989e69439f4e224328f7dacfc54ef
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-05-18 14:18:56 +0600

    wip

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=" font-serif">{children}</body>
+      <body className="">{children}</body>
     </html>
   );
 }
