141a06a5cdd7ca791c6fd647626f851ce0e7b497
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-05-21 22:09:54 +0600

    register page done

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -2,10 +2,12 @@
 "use client";
 import React, { useState } from "react";
 import InputField from "../../../components/common/InputField";
+import PrimaryButton from "../../../components/common/PrimaryButton";
 
 const Register = () => {
   const [form, setForm] = useState({
-    name: "",
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
     confirmPassword: "",
@@ -26,22 +28,30 @@ const Register = () => {
             alt=""
           />
 
-          <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5 shadow-xl">
+          <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5 shadow-xl rounded-tr-2xl rounded-br-2xl">
             <div class="w-full">
               <h1 class="text-2xl font-semibold  text-gray-800 capitalize">
                 Create your Babut Hut Account.
               </h1>
-              <form class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+              <form class="grid grid-cols-1 gap-4 mt-8 md:grid-cols-2">
                 <InputField
-                  label="Name"
-                  placeholder="First & Last Name"
-                  id="email"
+                  label="first Name"
+                  placeholder="First Name"
+                  id="firstName"
                   required
-                  value={form.name}
-                  name="name"
+                  value={form.firstName}
+                  name="firstName"
                   onChange={handleChange}
                 />
-
+                <InputField
+                  label="Last Name"
+                  placeholder="Last Name"
+                  id="lastName"
+                  required
+                  value={form.lastName}
+                  name="lastName"
+                  onChange={handleChange}
+                />
                 <InputField
                   label="Email Address"
                   required
@@ -75,23 +85,7 @@ const Register = () => {
                   name="confirmPassword"
                   onChange={handleChange}
                 />
-
-                <button class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                  <span>Sign Up </span>
-
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5 rtl:-scale-x-100"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </button>
+                <PrimaryButton value={"Register"} />
               </form>
             </div>
           </div>
