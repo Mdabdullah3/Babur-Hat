3fed6ab06dbe88a1a54ce2f71994b4c77f55455d
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-05-31 21:17:11 +0600

    up

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -5,16 +5,30 @@ import BarChart from "../../../components/Dashboard/Chart/BarChart";
 const page = () => {
   return (
     <section className="grid grid-cols-4 gap-5 rounded-xl">
-      <div className="mt-4 shadow-lg pl-2 h-64">
-        <h1 className="text-gray-500 text-lg flex items-center font-bold pt-3">
-          $ <span className="text-3xl text-black mr-2">56,9923</span>
-          <span className="flex items-center bg-green-200 text-sm rounded-lg px-2 py-1 text-green-600">
-            <AiOutlineArrowUp /> <span>+20%</span>
-          </span>
-        </h1>
-        <h2 className="text-md text-gray-500">Expected Earning</h2>
-        <div className="-mt-6">
-          <PieChart />
+      <div>
+        <div className="mt-4 shadow-lg pl-2 h-64">
+          <h1 className="text-gray-500 text-lg flex items-center font-bold pt-3">
+            $ <span className="text-3xl text-black mr-2">56,9923</span>
+            <span className="flex items-center bg-green-200 text-sm rounded-lg px-2 py-1 text-green-600">
+              <AiOutlineArrowUp /> <span>+20%</span>
+            </span>
+          </h1>
+          <h2 className="text-md text-gray-500">Expected Earning</h2>
+          <div className="-mt-10">
+            <PieChart />
+          </div>
+        </div>
+        <div>
+          <div className="mt-4 shadow-lg pl-2 h-64">
+            <h1 className="text-gray-500 text-lg flex items-center font-bold pt-3">
+              $ <span className="text-3xl text-black mr-2">56,9923</span>
+              <span className="flex items-center bg-green-200 text-sm rounded-lg px-2 py-1 text-green-600">
+                <AiOutlineArrowUp /> <span>+20%</span>
+              </span>
+            </h1>
+            <h2 className="text-md text-gray-500">Expected Earning</h2>
+            <div className=""></div>
+          </div>
         </div>
       </div>
       <div className="mt-4 shadow-lg pl-2 h-64 rounded-xl">
