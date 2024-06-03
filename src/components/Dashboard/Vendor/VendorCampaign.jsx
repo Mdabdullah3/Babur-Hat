b93a8ddbde0ac9238a0c0b856939b5a608799fcb
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-03 19:36:22 +0600

    up

--- src/components/Dashboard/Vendor/VendorCampaign.jsx
+++ src/components/Dashboard/Vendor/VendorCampaign.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { vendorEvent } from "../../../utils/constants";
+"use client";
+import React, { useState } from "react";
+import { vendorEvents } from "../../../utils/constants";
 import TableHead from "../../common/TableHead";
 const VendorCampaign = () => {
+  const [selectedMenu, setSelectedMenu] = useState(1);
+  const handleMenuClick = (id) => {
+    setSelectedMenu(id);
+  };
   const header = [
     "Event Id",
     "Payment",
@@ -10,11 +15,24 @@ const VendorCampaign = () => {
     "Status",
     "Action",
   ];
+  const menu = [
+    {
+      id: 1,
+      name: "All",
+      items: 5,
+    },
+    {
+      id: 2,
+      name: "Previous Joined Event",
+      items: 10,
+    },
+  ];
   return (
-    <section>
-      {vendorEvent.length > 0 ? (
+    <section className="w-11/12 mx-auto">
+      <div></div>
+      {vendorEvents.length > 0 ? (
         <div>
-            <TableHead />
+          <TableHead header={header} />
         </div>
       ) : (
         <div className="text-center">No data found</div>
