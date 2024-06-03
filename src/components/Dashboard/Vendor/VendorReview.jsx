d08966bc337ef27f7703febe5e28f719c325b043
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-03 20:48:26 +0600

    up

--- src/components/Dashboard/Vendor/VendorReview.jsx
+++ src/components/Dashboard/Vendor/VendorReview.jsx
@@ -8,7 +8,7 @@ const VendorReview = () => {
     <section>
       <div className="flex mx-5 items-center justify-between mt-10">
         <h1 className="font-bold text-2xl">Review</h1>
-        <div className="flex items-center gap-2 text-white">
+        <div className="flex items-center gap-4 text-white">
           <h1 className="px-2 py-2 bg-primary text-white rounded-full">
             <IoIosArrowBack size={30} color="white" />
           </h1>
@@ -19,20 +19,32 @@ const VendorReview = () => {
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-10 mt-10">
         {vendorReviews.map((item) => (
-          <div key={item.id} className="shadow-xl px-4 py-6 rounded-xl">
+          <div
+            key={item.id}
+            className="shadow-xl px-4 py-6 rounded-xl flex justify-between items-center"
+          >
             <div>
-              <h1>{item.name}</h1>
+              <h1 className="text-sm text-gray-400">{item.date}</h1>
+              <h1>{item.review}</h1>
               <h1 className="flex items-center text-sm my-2 text-yellow-400">
                 <FaStar />
                 <FaStar />
                 <FaStar />
                 <FaStar />
               </h1>
-              <h1>{item.date}</h1>
-              <h1>{item.review}</h1>
+              <button className="px-5 py-2 bg-primary text-white rounded-full">
+                Edit
+              </button>
             </div>
             <div>
-              <img src={item.img} alt="review img" className="w-40" />
+              <h1 className="font-bold text-sm tracking-wider py-2">
+                {item.name}
+              </h1>
+              <img
+                src={item.img}
+                alt="review img"
+                className="w-16 rounded-full"
+              />
             </div>
           </div>
         ))}
