9a1bf696108ea343f9a0f8c074672857a066d991
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-03 20:52:36 +0600

    up

---
+++ src/components/Dashboard/Vendor/VendorSetting.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const VendorSetting = () => {
+    const menu = [
+        {
+            id: 1,
+            name: "Seller Info",
+        },
+        {
+            id: 2,
+            name: "Business Info",
+        },
+        {
+            id: 3,
+            name: "Bank Info"
+        },
+        {
+            id: 4,
+            name: "Warehouse Info"
+        },
+        {
+            id: 5,
+            name: "Return Address"
+        }
+    ]
+    return (
+        <section>
+            
+        </section>
+    );
+};
+
+export default VendorSetting;
