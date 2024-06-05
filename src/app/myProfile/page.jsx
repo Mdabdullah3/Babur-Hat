d5224b086c663dd1cc52c7fe0f646cb39861c223
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-05 21:44:23 +0600

    up

---
+++ src/app/myProfile/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MyProfile = () => {
+  const menu = [
+    "My Profile",
+    "My Orders",
+    "My Reviews",
+    "Recent Product",
+    "Logout",
+  ];
+  return <section></section>;
+};
+
+export default MyProfile;
