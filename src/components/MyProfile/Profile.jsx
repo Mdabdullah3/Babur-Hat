3bd8426c8403292250f9797174c720b2776f96e2
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-05 22:13:21 +0600

    up

--- src/components/MyProfile/Profile.jsx
+++ src/components/MyProfile/Profile.jsx
@@ -3,26 +3,26 @@ import React from "react";
 const Profile = () => {
   return (
     <section>
-      <h1>My Profile</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3">
+      <h1 className="text-3xl text-gray-700 mb-10 font-bold">My Profile</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 space-y-5">
         <div>
-          <h1>Full Name</h1>
+          <h1 className="font-bold mb-1">Full Name</h1>
           <h2>Md Abdullah</h2>
         </div>
         <div>
-          <h1>Email Address</h1>
+          <h1 className="font-bold mb-1">Email Address</h1>
           <h2>abdullah@gmail.com</h2>
         </div>
         <div>
-          <h1>Mobile</h1>
+          <h1 className="font-bold mb-1">Mobile</h1>
           <h2>0123456789</h2>
         </div>
         <div>
-          <h1>Address</h1>
+          <h1 className="font-bold mb-1">Address</h1>
           <h2>Dhaka, Bangladesh</h2>
         </div>
         <div>
-          <h1>Gender</h1>
+          <h1 className="font-bold mb-1">Gender</h1>
           <h2>Male</h2>
         </div>
       </div>
