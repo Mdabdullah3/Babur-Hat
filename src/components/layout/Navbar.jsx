cc334e1dadef9e8c1f2ea0735c0d5f5e4e1bdd9b
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-18 21:32:00 +0600

    responsive navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -48,7 +48,9 @@ const Navbar = () => {
               <MdMenu size={32} />
             </label>
             <Link href="/" className="text-3xl font-bold cursor-pointer">
-              <h1 className="text-3xl font-bold cursor-pointer">Babur Hut</h1>
+              <h1 className="lg:text-3xl text-xl font-bold cursor-pointer">
+                Babur Hut
+              </h1>
             </Link>
           </div>
           <div className="hidden md:block">
@@ -65,11 +67,11 @@ const Navbar = () => {
           </div>
           <div className="flex items-center gap-8">
             <div className="flex items-center gap-4">
-              <div>
-                <FaRegUser size={30} />
-              </div>
+              <h1 className="lg:text-3xl text-2xl">
+                <FaRegUser />
+              </h1>
               {displayUser ? (
-                <div className="tracking-wider">
+                <div className="tracking-wider hidden lg:block">
                   <h1 className="text-[13px]">Welcome</h1>
                   <div className="dropdown dropdown-hover">
                     <div
@@ -96,7 +98,7 @@ const Navbar = () => {
                   </div>
                 </div>
               ) : (
-                <div className="tracking-wider">
+                <div className="tracking-wider hidden lg:block">
                   <h1 className="text-[13px]">Welcome</h1>
                   <div className="dropdown dropdown-hover relative">
                     <div
@@ -124,7 +126,7 @@ const Navbar = () => {
               className="flex items-center gap-4 cursor-pointer"
             >
               <PiShoppingCartSimpleBold size={32} />
-              <div>
+              <div className="hidden md:block">
                 <h1 className="bg-white px-3 rounded-full py-0 text-black">
                   {cart?.length || 0}
                 </h1>
@@ -142,15 +144,15 @@ const Navbar = () => {
             <li>Top Rated</li>
           </ul>
         </div>
-        <div className="block md:hidden mt-4 px-4">
-          <label className="input input-bordered rounded-full flex items-center gap-2 h-[52px] mx-auto">
+        <div className="block md:hidden lg:mt-4 mt-2 px-4">
+          <label className="input input-bordered rounded-full flex items-center gap-2 lg:h-[52px] h-[44px] mx-auto">
             <input
               type="text"
               className="grow w-full"
               placeholder="Bluetooth Headphones"
             />
-            <div className="bg-black px-6 py-2 rounded-full">
-              <FiSearch className="text-white" size={29} />
+            <div className="bg-black px-4 py-2 rounded-full">
+              <FiSearch className="text-white text-xl lg:text-2xl" />
             </div>
           </label>
         </div>
