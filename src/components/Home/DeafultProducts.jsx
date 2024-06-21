2260eb1893597f113a64ac750613bd71dc7d805e
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-21 21:53:37 +0600

    up

--- src/components/Home/DeafultProducts.jsx
+++ src/components/Home/DeafultProducts.jsx
@@ -10,8 +10,8 @@ const DeafultProducts = () => {
       .then((data) => setProducts(data));
   }, []);
   return (
-    <div className="w-11/12 mx-auto mt-10">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
+    <div className="lg:w-11/12 w-[95%] mx-auto mt-4 lg:mt-10">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 lg:gap-8">
         {Products?.products?.slice(0, 12).map((product) => (
           <ProductCard product={product} key={product?._id} />
         ))}
