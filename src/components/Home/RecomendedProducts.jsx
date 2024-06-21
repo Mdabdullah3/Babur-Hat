4b54d2e3db5953d9e1b1b1c881ab0abe6bf2b91e
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-21 21:30:27 +0600

    responsive

--- src/components/Home/RecomendedProducts.jsx
+++ src/components/Home/RecomendedProducts.jsx
@@ -10,11 +10,11 @@ const RecomendedProducts = () => {
       .then((data) => setProducts(data));
   }, []);
   return (
-    <div className="w-11/12 mx-auto mt-2 lg:mt-10">
+    <div className="lg:w-11/12 w-[95%] mx-auto mt-2 lg:mt-10">
       <h1 className="text-lg lg:text-2xl lg:mb-10 mb-4 font-bold">
         Recomended Products
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 lg:gap-8">
         {Products?.products?.slice(0, 12).map((product) => (
           <ProductCard product={product} key={product._id} />
         ))}
--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -18,33 +18,45 @@ const ProductCard = ({ product }) => {
   };
   return (
     <section
-      className="relative hover:shadow-md px-4 py-4 transition duration-300 rounded-2xl hover:bg-white"
+      className="relative hover:shadow-md px-2 py-4 transition duration-300 rounded-2xl hover:bg-white"
       key={product._id}
     >
-      <Link href={`/products/${product._id}`} className="mb-4  cursor-pointer ">
+      <Link
+        href={`/products/${product._id}`}
+        className="lg:mb-4  cursor-pointer "
+      >
         <div className="">
           <img
-            className="md:w-64 w-56 h-44 md:h-52 "
+            className="md:w-64 w-64 h-40 md:h-52 "
             src={product.topimg}
             alt=""
           />
         </div>
-        <h1 className="text-md">{product.productName.slice(0, 20)}...</h1>
-        <h1 className="flex items-center text-sm my-2">
-          <FaStar />
-          <FaStar />
-          <FaStar />
-          <FaStar />
-          <span className="text-gray-500 text-sm ml-3">1000+ Sold</span>
+        <h1 className="text-[14px] md:text-md">
+          {product.productName.slice(0, 20)}...
         </h1>
-        <h1 className="text-xl font-bold">
-          <span className="text-sm">BDT</span> {product.sellPrice}.00{" "}
-          <del className="font-normal text-sm text-gray-400">BDT 535.00</del>
+        <h1 className="flex items-center  justify-between lg:text-sm text-[14px] text-yellow-500 my-2">
+          <div className="flex gap-1">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+          <span className="text-gray-500 lg:text-sm text-[12px] lg:ml-3">
+            1000+ Sold
+          </span>
+        </h1>
+        <h1 className="lg:text-xl text-[14px] font-bold">
+          <span className="lg:text-sm text-[14px]">BDT</span>{" "}
+          {product.sellPrice}.00{" "}
+          <del className="font-normal ml-2 lg:text-sm text-gray-400 text-[14px]">
+            535.00
+          </del>
         </h1>
       </Link>
       <div
         onClick={handleAddToCart}
-        className="absolute bottom-32 md:bottom-28 right-5 lg:px-3 px-2 py-2 lg:py-3 rounded-full bg-black text-white tooltip tooltip-left text-xl lg:text-2xl"
+        className="absolute bottom-20 lg:bottom-28 right-5 lg:px-3 px-2 py-2 lg:py-3 rounded-full bg-black text-white tooltip tooltip-left text-xl lg:text-2xl"
         data-tip="Add To Cart"
       >
         <LiaCartPlusSolid />
