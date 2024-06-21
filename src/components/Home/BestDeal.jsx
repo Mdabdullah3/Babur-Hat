b4fb1ab990500e0374af9013004f5d5d882e2918
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-21 21:51:34 +0600

    wip

--- src/components/Home/BestDeal.jsx
+++ src/components/Home/BestDeal.jsx
@@ -16,8 +16,10 @@ const BestDeal = () => {
   }, []);
 
   return (
-    <div className="w-11/12 mx-auto mt-16 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500   rounded-2xl px-4 py-5">
-      <h1 className="text-2xl font-bold ml-4 mb-8 text-white">Best Deal</h1>
+    <div className="lg:w-11/12 w-[95%] mx-auto mt-4 lg:mt-16 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500   rounded-2xl lg:p-4 p-3">
+      <h1 className="lg:text-2xl text-lg font-bold lg:ml-4 lg:mb-8 mb-4 text-white">
+        Best Deal
+      </h1>
       <div>
         <Swiper
           slidesPerView={2}
@@ -52,23 +54,27 @@ const BestDeal = () => {
         >
           {bestDeal?.map((item, index) => (
             <SwiperSlide key={index}>
-              <div className="slide-content bg-white mx-3 rounded-2xl h-[360px]">
+              <div className="slide-content bg-white lg:mx-3 mr-1.5 rounded-2xl md:h-[360px] pb-4">
                 <img
                   src={item?.image}
                   className="rounded-2xl w-full cursor-pointer"
                   alt="Best Deal Image"
                 />
                 <div className="-mt-4 px-4">
-                  <h1 className="text-md">{item?.title}</h1>
-                  <del className="my-3 text-gray-400">BDT 423.00</del>
-                  <h1 className="font-bold mb-2">BDT {item?.price}.00</h1>
-                  <div className="flex items-center justify-between">
+                  <h1 className="lg:text-md text-[12px">{item?.title}</h1>
+                  <del className="my-3 text-gray-400 text-sm">BDT 423.00</del>
+                  <h1 className="font-bold text-sm mb-2">
+                    BDT {item?.price}.00
+                  </h1>
+                  <div className="flex mt-1 items-center justify-between">
                     <progress
                       className="progress progress-primary w-24"
                       value="80"
                       max="100"
                     ></progress>
-                    <h1 className="text-sm text-gray-500">50 Pc Left</h1>
+                    <h1 className="g:text-sm hidden md:block text-gray-500">
+                      50 Pc Left
+                    </h1>
                   </div>
                 </div>
               </div>
