8530ae5a0eceff24e14288a1990b6bcc83bbb581
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-07-16 22:45:00 +0600

    up

--- src/components/Home/BannerCategory.jsx
+++ src/components/Home/BannerCategory.jsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
 import { category } from "../../utils/constants";
 
 const BannerCategory = () => {
   return (
     <div className="bg-white rounded-2xl px-6 text-md pb-2 border-[1px] border-gray-300 shadow-md">
       {category.map((cat, index) => (
-        <div
+        <Link
+          href="/shop"
           key={index}
           className="flex items-center gap-3 mt-4 tracking-wider text-gray-600"
         >
           <h1 className="bg-gray-200 px-1 py-1 rounded-full">{cat?.icon}</h1>
           <h1>{cat?.name}</h1>
-        </div>
+        </Link>
       ))}
     </div>
   );
