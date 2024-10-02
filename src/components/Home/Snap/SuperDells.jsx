6d2a033f3d6c468beacc4a7cd86805953a5de0f1
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-10-02 12:57:29 +0600

    event load done

--- src/components/Home/Snap/SuperDells.jsx
+++ src/components/Home/Snap/SuperDells.jsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react";
 import useEventStore from "../../../store/eventStore";
 import { SERVER } from "../../../config";
-
 const SuperDells = () => {
   const { packageProducts, fetchPackageProducts } = useEventStore();
   const [loading, setLoading] = useState(true);
@@ -16,58 +15,58 @@ const SuperDells = () => {
     fetchData();
   }, [fetchPackageProducts]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // if (loading) {
+  //     return <div>Loading...</div>;
+  //   }
 
-  console.log(packageProducts);
+  console.log(
+    `${SERVER}${packageProducts[0]?.product?.coverPhoto?.secure_url}`
+  );
   return (
     <div className="bg-rose-200/60 px-6 py-4 rounded-2xl w-96">
       <h1 className="text-2xl text-primary font-bold mb-4">Super Dells</h1>
       <div className="bg-white rounded-2xl pb-2">
-        {packageProducts[0]?.product?.image?.secure_url && (
-          <img
-            src={`${SERVER}${packageProducts[0]?.product?.image?.secure_url}`}
-            className="rounded-2xl mx-auto w-60"
-            alt="Super Dells"
-          />
-        )}
-        <h1 className="text-primary text-md text-center">
-          BDT{" "}
-          <span className="text-2xl font-bold">
-            {packageProducts[0]?.product?.price}
-          </span>
-          <span className="bg-primary text-white px-1 py-1 text-lg rounded ml-1">
-            -20%
-          </span>
+        <img
+          src={`${SERVER}${packageProducts[0]?.product?.coverPhoto?.secure_url}`}
+          className="rounded-2xl mx-auto w-60"
+          alt="Super Dells"
+        />
+        <h1 className="text-xl font-bold text-primary text-center mt-2">
+          BDT
+          {packageProducts[0]?.product?.productVariants[0]?.discount > 0
+            ? packageProducts[0]?.product?.productVariants[0]?.discount
+            : packageProducts[0]?.product?.productVariants[0]?.price}
+          .00
+          <del className="ml-2 font-normal text-gray-400 text-lg">
+            BDT
+            {packageProducts[0]?.product?.productVariants[0]?.discount &&
+              packageProducts[0]?.product?.productVariants[0]?.price}
+            .00
+          </del>
         </h1>
         <div className="flex items-center justify-center gap-3">
-          <div>
-            <img
-              src="https://ae01.alicdn.com/kf/Sd20b194de16346a2a94b5266cce21f65v.jpg_480x480.jpg_.webp"
-              className="w-36"
-              alt="sd"
-            />
-            <h1 className="text-primary text-md text-center">
-              BDT <span className="text-2xl font-bold">23.00</span>
-              <span className="bg-primary text-white px-1 py-1 text-lg rounded ml-1">
-                -20%
-              </span>
-            </h1>
-          </div>
-          <div>
-            <img
-              src="https://ae01.alicdn.com/kf/S28738cdae6b343fbb0bc833a1de001f8B.jpg_480x480.jpg_.webp"
-              alt="sd"
-              className="w-36"
-            />
-            <h1 className="text-primary text-md text-center">
-              BDT <span className="text-2xl font-bold">23.00</span>
-              <span className="bg-primary text-white px-1 py-1 text-lg rounded">
-                -20%
-              </span>
-            </h1>
-          </div>
+          {packageProducts?.slice(0, 2).map((item) => (
+            <>
+              <div>
+                <img
+                  src={`${SERVER}${item?.product?.coverPhoto?.secure_url}`}
+                  alt="sd"
+                  className="w-36 rounded-md"
+                />
+                <h1 className="text-md  text-primary text-center mt-2">
+                  BDT
+                  {item?.product?.productVariants[0]?.discount > 0
+                    ? item?.product?.productVariants[0]?.discount
+                    : item?.product?.productVariants[0]?.price}
+                  <del className="ml-2 font-normal text-gray-400 text-sm">
+                    BDT
+                    {item?.product?.productVariants[0]?.discount &&
+                      item?.product?.productVariants[0]?.price}
+                  </del>
+                </h1>
+              </div>
+            </>
+          ))}
         </div>
       </div>
     </div>
