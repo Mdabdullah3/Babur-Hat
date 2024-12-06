41b1471e39c2d323bf2d291bdb7f310069badb7a
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-12-06 17:16:36 +0600

    domain change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const PRODUCTION = true
-const PRODUCTION_API = 'https://baburhaatbd.com/api'
+const PRODUCTION_API = 'https://readyhow.com/api'
 const DEVELOPMENT_API = 'http://localhost:5000/api'
 export const API_URL = PRODUCTION ? PRODUCTION_API : DEVELOPMENT_API
-export const SERVER = PRODUCTION ? 'https://baburhaatbd.com' : 'http://localhost:5000'
+export const SERVER = PRODUCTION ? 'https://readyhow.com' : 'http://localhost:5000'
