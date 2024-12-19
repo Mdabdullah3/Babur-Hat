c892cd4fc617643858273b819746fc6493a3d618
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-12-19 15:06:19 +0600

    up

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,103 +1,116 @@
 import React from "react";
+import Navbar from "../../components/layout/Navbar";
 
 export default function About() {
   return (
-    <div className="bg-gray-50 text-gray-800">
-      <header className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-12">
-        <div className="container mx-auto px-4 text-center">
-          <h1 className="text-4xl font-bold mb-4">About Us</h1>
-          <p className="text-lg">
-            Discover the essence of Bangladeshi fashion with Ready How.
-          </p>
-        </div>
-      </header>
+    <main>
+      <nav>
+        <Navbar />
+      </nav>
+      <div className="bg-gray-50 text-gray-800">
+        <header className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-12">
+          <div className="container mx-auto px-4 text-center">
+            <h1 className="text-4xl font-bold mb-4">About Us</h1>
+            <p className="text-lg">
+              Discover the essence of Bangladeshi fashion with Ready How.
+            </p>
+          </div>
+        </header>
 
-      <section className="py-16">
-        <div className="container mx-auto px-4 text-center">
-          <h2 className="text-3xl font-bold mb-6">Welcome to Ready How</h2>
-          <p className="text-lg text-gray-600 leading-relaxed">
-            Ready How is the first and only fashion-focused e-commerce platform
-            in Bangladesh, bringing together the best of Bangladeshi fashion
-            under one roof. Whether youre looking for trendy apparel, stylish
-            accessories, or timeless traditional wear, we are your go-to
-            destination for everything fashion.
-          </p>
-        </div>
-      </section>
+        <section className="py-16">
+          <div className="container mx-auto px-4 text-center">
+            <h2 className="text-3xl font-bold mb-6">Welcome to Ready How</h2>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              Ready How is the first and only fashion-focused e-commerce
+              platform in Bangladesh, bringing together the best of Bangladeshi
+              fashion under one roof. Whether youre looking for trendy apparel,
+              stylish accessories, or timeless traditional wear, we are your
+              go-to destination for everything fashion.
+            </p>
+          </div>
+        </section>
 
-      <section className="py-16 bg-gray-100">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-6">Our Mission</h2>
-          <p className="text-lg text-center text-gray-600 leading-relaxed max-w-2xl mx-auto">
-            To make fashion accessible, affordable, and sustainable for
-            everyone, while showcasing the finest Bangladeshi brands to the
-            world.
-          </p>
-        </div>
-      </section>
+        <section className="py-16 bg-gray-100">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold text-center mb-6">Our Mission</h2>
+            <p className="text-lg text-center text-gray-600 leading-relaxed max-w-2xl mx-auto">
+              To make fashion accessible, affordable, and sustainable for
+              everyone, while showcasing the finest Bangladeshi brands to the
+              world.
+            </p>
+          </div>
+        </section>
 
-      <section className="py-16">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-6">What We Offer</h2>
-          <div className="grid md:grid-cols-3 gap-8 text-center">
-            <div className="bg-white shadow-lg p-6 rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">Premium Brands</h3>
-              <p className="text-gray-600">
-                Top names in Bangladeshi fashion, known for their superior
-                quality and iconic designs.
-              </p>
-            </div>
-            <div className="bg-white shadow-lg p-6 rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">Established Stores</h3>
-              <p className="text-gray-600">
-                Physical shops with limited online presence, offering unique
-                products and local charm.
-              </p>
-            </div>
-            <div className="bg-white shadow-lg p-6 rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">Online-Only Brands</h3>
-              <p className="text-gray-600">
-                Emerging digital-first brands that bring fresh styles to the
-                table.
-              </p>
+        <section className="py-16">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold text-center mb-6">
+              What We Offer
+            </h2>
+            <div className="grid md:grid-cols-3 gap-8 text-center">
+              <div className="bg-white shadow-lg p-6 rounded-lg">
+                <h3 className="text-xl font-semibold mb-4">Premium Brands</h3>
+                <p className="text-gray-600">
+                  Top names in Bangladeshi fashion, known for their superior
+                  quality and iconic designs.
+                </p>
+              </div>
+              <div className="bg-white shadow-lg p-6 rounded-lg">
+                <h3 className="text-xl font-semibold mb-4">
+                  Established Stores
+                </h3>
+                <p className="text-gray-600">
+                  Physical shops with limited online presence, offering unique
+                  products and local charm.
+                </p>
+              </div>
+              <div className="bg-white shadow-lg p-6 rounded-lg">
+                <h3 className="text-xl font-semibold mb-4">
+                  Online-Only Brands
+                </h3>
+                <p className="text-gray-600">
+                  Emerging digital-first brands that bring fresh styles to the
+                  table.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="py-16 bg-gray-100">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-6">
-            Why Choose Us?
-          </h2>
-          <ul className="space-y-4 text-gray-600 max-w-2xl mx-auto">
-            <li>
-              <span className="font-semibold">Comprehensive Selection:</span>{" "}
-              All prominent Bangladeshi fashion brands, available in one place.
-            </li>
-            <li>
-              <span className="font-semibold">Convenience:</span> Seamless
-              shopping experience via our website and mobile apps on iOS and
-              Android.
-            </li>
-            <li>
-              <span className="font-semibold">Affordability:</span> Competitive
-              pricing with exclusive discounts and promotions.
-            </li>
-            <li>
-              <span className="font-semibold">Global Reach:</span> We bring
-              Bangladeshi fashion to customers worldwide, ensuring high-quality
-              garments at affordable prices.
-            </li>
-          </ul>
-        </div>
-      </section>
+        <section className="py-16 bg-gray-100">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold text-center mb-6">
+              Why Choose Us?
+            </h2>
+            <ul className="space-y-4 text-gray-600 max-w-2xl mx-auto">
+              <li>
+                <span className="font-semibold">Comprehensive Selection:</span>{" "}
+                All prominent Bangladeshi fashion brands, available in one
+                place.
+              </li>
+              <li>
+                <span className="font-semibold">Convenience:</span> Seamless
+                shopping experience via our website and mobile apps on iOS and
+                Android.
+              </li>
+              <li>
+                <span className="font-semibold">Affordability:</span>{" "}
+                Competitive pricing with exclusive discounts and promotions.
+              </li>
+              <li>
+                <span className="font-semibold">Global Reach:</span> We bring
+                Bangladeshi fashion to customers worldwide, ensuring
+                high-quality garments at affordable prices.
+              </li>
+            </ul>
+          </div>
+        </section>
 
-      <footer className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-8">
-        <div className="container mx-auto px-4 text-center">
-          <p>&copy; 2024 Ready How. All Rights Reserved.</p>
-        </div>
-      </footer>
-    </div>
+        <footer className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-8">
+          <div className="container mx-auto px-4 text-center">
+            <p>&copy; 2024 Ready How. All Rights Reserved.</p>
+          </div>
+        </footer>
+      </div>
+    </main>
   );
 }
--- src/app/terms-and-condition/page.jsx
+++ src/app/terms-and-condition/page.jsx
@@ -1,154 +1,161 @@
 import React from "react";
+import Navbar from "../../components/layout/Navbar";
 
 const TermsAndConditions = () => {
   return (
-    <div className="bg-gray-100 py-10">
-      <div className="max-w-6xl mx-auto bg-white p-8 shadow-md rounded-md">
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
-          Terms and Conditions
-        </h1>
+    <section>
+      <nav>
+        <Navbar />
+      </nav>
+      <div className="bg-gray-100 py-10">
+        <div className="max-w-6xl mx-auto bg-white p-8 shadow-md rounded-md">
+          <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+            Terms and Conditions
+          </h1>
 
-        <p className="text-gray-600 mb-6">
-          Welcome to Ready How! Thank you for choosing Ready How, your trusted
-          platform for accessing a diverse range of Bangladeshi fashion brands.
-          These Terms and Conditions Terms govern your use of our website,
-          mobile applications, and associated services. By engaging with Ready
-          How, you signify your acceptance of these Terms. Please read them
-          carefully before proceeding.
-        </p>
+          <p className="text-gray-600 mb-6">
+            Welcome to Ready How! Thank you for choosing Ready How, your trusted
+            platform for accessing a diverse range of Bangladeshi fashion
+            brands. These Terms and Conditions Terms govern your use of our
+            website, mobile applications, and associated services. By engaging
+            with Ready How, you signify your acceptance of these Terms. Please
+            read them carefully before proceeding.
+          </p>
 
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            1. General Overview
-          </h2>
-          <ul className="list-disc ml-6 text-gray-600">
-            <li>
-              <strong>About Ready How:</strong> Ready How operates as a digital
-              marketplace connecting vendors and customers to facilitate the
-              buying and selling of fashion products. We do not own or stock
-              inventory but provide a secure platform for seamless transactions.
-            </li>
-            <li>
-              <strong>Acceptance of Terms:</strong> By accessing or using our
-              services, you confirm your agreement to these Terms. This
-              agreement is binding, subject to the laws of Bangladesh.
-            </li>
-            <li>
-              <strong>Modifications to Terms:</strong> Ready How reserves the
-              right to amend these Terms at any time. Changes will take effect
-              immediately upon posting.
-            </li>
-            <li>
-              <strong>Eligibility to Use:</strong> To register and shop on Ready
-              How, you must be at least 18 years old or supervised by a
-              parent/legal guardian if under 18.
-            </li>
-            <li>
-              <strong>Privacy Policy:</strong> Your use of Ready How is also
-              governed by our Privacy Policy, which outlines how we collect,
-              use, and protect your personal information.
-            </li>
-          </ul>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+              1. General Overview
+            </h2>
+            <ul className="list-disc ml-6 text-gray-600">
+              <li>
+                <strong>About Ready How:</strong> Ready How operates as a
+                digital marketplace connecting vendors and customers to
+                facilitate the buying and selling of fashion products. We do not
+                own or stock inventory but provide a secure platform for
+                seamless transactions.
+              </li>
+              <li>
+                <strong>Acceptance of Terms:</strong> By accessing or using our
+                services, you confirm your agreement to these Terms. This
+                agreement is binding, subject to the laws of Bangladesh.
+              </li>
+              <li>
+                <strong>Modifications to Terms:</strong> Ready How reserves the
+                right to amend these Terms at any time. Changes will take effect
+                immediately upon posting.
+              </li>
+              <li>
+                <strong>Eligibility to Use:</strong> To register and shop on
+                Ready How, you must be at least 18 years old or supervised by a
+                parent/legal guardian if under 18.
+              </li>
+              <li>
+                <strong>Privacy Policy:</strong> Your use of Ready How is also
+                governed by our Privacy Policy, which outlines how we collect,
+                use, and protect your personal information.
+              </li>
+            </ul>
+          </section>
 
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            2. Account Registration and Management
-          </h2>
-          <ul className="list-disc ml-6 text-gray-600">
-            <li>
-              <strong>Creating an Account:</strong> To access exclusive features
-              like order tracking, wishlists, and app-exclusive discounts, you
-              need to create a user account.
-            </li>
-            <li>
-              <strong>Responsibilities of Account Holders:</strong> As an
-              account holder, you are solely responsible for activities
-              conducted under your account.
-            </li>
-            <li>
-              <strong>Account Termination:</strong> Ready How reserves the right
-              to suspend or terminate accounts that violate these Terms.
-            </li>
-            <li>
-              <strong>Dormant Accounts:</strong> Accounts inactive for a
-              prolonged period (e.g., 12 months) may be deactivated after
-              notification.
-            </li>
-          </ul>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+              2. Account Registration and Management
+            </h2>
+            <ul className="list-disc ml-6 text-gray-600">
+              <li>
+                <strong>Creating an Account:</strong> To access exclusive
+                features like order tracking, wishlists, and app-exclusive
+                discounts, you need to create a user account.
+              </li>
+              <li>
+                <strong>Responsibilities of Account Holders:</strong> As an
+                account holder, you are solely responsible for activities
+                conducted under your account.
+              </li>
+              <li>
+                <strong>Account Termination:</strong> Ready How reserves the
+                right to suspend or terminate accounts that violate these Terms.
+              </li>
+              <li>
+                <strong>Dormant Accounts:</strong> Accounts inactive for a
+                prolonged period (e.g., 12 months) may be deactivated after
+                notification.
+              </li>
+            </ul>
+          </section>
 
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            3. Products and Vendors
-          </h2>
-          <ul className="list-disc ml-6 text-gray-600">
-            <li>
-              <strong>Role of Ready How:</strong> We act as an intermediary
-              between customers and vendors, facilitating transactions.
-            </li>
-            <li>
-              <strong>Vendor Responsibilities:</strong> Vendors are responsible
-              for product descriptions, quality, and warranties.
-            </li>
-            <li>
-              <strong>Product Availability:</strong> Products are subject to
-              availability. If out of stock, alternatives or refunds will be
-              provided.
-            </li>
-            <li>
-              <strong>Specialty Products:</strong> Custom terms may apply for
-              categories like sustainable fashion or luxury items.
-            </li>
-          </ul>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+              3. Products and Vendors
+            </h2>
+            <ul className="list-disc ml-6 text-gray-600">
+              <li>
+                <strong>Role of Ready How:</strong> We act as an intermediary
+                between customers and vendors, facilitating transactions.
+              </li>
+              <li>
+                <strong>Vendor Responsibilities:</strong> Vendors are
+                responsible for product descriptions, quality, and warranties.
+              </li>
+              <li>
+                <strong>Product Availability:</strong> Products are subject to
+                availability. If out of stock, alternatives or refunds will be
+                provided.
+              </li>
+              <li>
+                <strong>Specialty Products:</strong> Custom terms may apply for
+                categories like sustainable fashion or luxury items.
+              </li>
+            </ul>
+          </section>
 
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            4. Pricing and Payment
-          </h2>
-          <ul className="list-disc ml-6 text-gray-600">
-            <li>
-              <strong>Pricing Transparency:</strong> Prices include taxes unless
-              stated otherwise.
-            </li>
-            <li>
-              <strong>Accepted Payment Methods:</strong> Mobile wallets, cards,
-              bank transfers, and cash on delivery are supported.
-            </li>
-            <li>
-              <strong>Failed Payments:</strong> Orders with failed payments will
-              not be processed until resolved.
-            </li>
-          </ul>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+              4. Pricing and Payment
+            </h2>
+            <ul className="list-disc ml-6 text-gray-600">
+              <li>
+                <strong>Pricing Transparency:</strong> Prices include taxes
+                unless stated otherwise.
+              </li>
+              <li>
+                <strong>Accepted Payment Methods:</strong> Mobile wallets,
+                cards, bank transfers, and cash on delivery are supported.
+              </li>
+              <li>
+                <strong>Failed Payments:</strong> Orders with failed payments
+                will not be processed until resolved.
+              </li>
+            </ul>
+          </section>
 
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            5. Shipping and Delivery
-          </h2>
-          <ul className="list-disc ml-6 text-gray-600">
-            <li>
-              <strong>Delivery Areas:</strong> Ready How delivers within
-              Bangladesh, with plans for international expansion.
-            </li>
-            <li>
-              <strong>Estimated Delivery Times:</strong> Timelines depend on
-              vendor processing and location.
-            </li>
-            <li>
-              <strong>Missed Deliveries:</strong> Incorrect details or
-              unavailability may result in additional charges.
-            </li>
-          </ul>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+              5. Shipping and Delivery
+            </h2>
+            <ul className="list-disc ml-6 text-gray-600">
+              <li>
+                <strong>Delivery Areas:</strong> Ready How delivers within
+                Bangladesh, with plans for international expansion.
+              </li>
+              <li>
+                <strong>Estimated Delivery Times:</strong> Timelines depend on
+                vendor processing and location.
+              </li>
+              <li>
+                <strong>Missed Deliveries:</strong> Incorrect details or
+                unavailability may result in additional charges.
+              </li>
+            </ul>
+          </section>
 
-        <p className="text-gray-600 text-sm text-center mt-6">
-          By using Ready How, you agree to these Terms and Conditions. For
-          detailed policies, please contact our support team.
-        </p>
+          <p className="text-gray-600 text-sm text-center mt-6">
+            By using Ready How, you agree to these Terms and Conditions. For
+            detailed policies, please contact our support team.
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
